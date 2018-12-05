31a3b178149f5a1e8c18d68fab77d354518c1b77
Asim Zaidi <asimzaidih@gmail.com>
2018-12-05 01:55:44 -0800

    refactor, fixing broken links, particles running

---
+++ public/js/index-particle.js
@@ -0,0 +1,34 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+        "value": 40,
+        "density": {
+            "enable": true,
+            "value_area": 800
+        }
+        },
+        "color": {
+        "value": [ "#00FF00", "#1E8FFF", "#FF0000" ]
+        },
+        "shape": {
+        "type": "circle"
+        },
+        "opacity": {
+        "value": 0.5,
+        "random": true,
+        },
+        "size": {
+        "value": 9
+        },
+        "line_linked": {
+        "enable": true,
+        "distance": 70
+        },
+        "move": {
+        "enable": true,
+        "speed": 4
+        }
+    }
+});
+
+// requestAnimationFrame();
